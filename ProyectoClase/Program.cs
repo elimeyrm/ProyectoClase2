3861096e26143094a393b461a42cf8b242f8e465
elimeyrm <84092811+elimeyrm@users.noreply.github.com>
2023-05-05 15:32:00 -0400

    Inicio del proyecto clase

---
+++ ProyectoClase/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.Title = "Uso de un procedimiento";
+            int num;
+            Console.WriteLine("Digitar un numero entero positivo:");
+            num = int.Parse(Console.ReadLine());
+            Par(num);
+            Console.WriteLine("\n");
+            Console.WriteLine("-->Fin del programa");
+            Console.ReadKey();
+        }
+
+        static void Par(int a)
+        {
+            int x;
+            x = (a % 2);
+            if (x == 0)
+            {
+                Console.WriteLine("\nEs par");
+            }
+            else
+            {
+                Console.WriteLine("\nEs impar");
+            }
+        }
+
+    }
+}
---
+++ ProyectoClase/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HolaMundo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HolaMundo")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d0213702-2369-45ea-9dcc-35a46c94fcb2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
